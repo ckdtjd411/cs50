41f224fa719f1f5efca64bbb28507108d935517b
ckdtjd411 <ckdtjd411@hanmail.net>
2020-08-17 17:41:09 +0900

    exercise

---
+++ 5. 메모리/실습/address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    char *s = "EMMA";
+    
+    printf("%s\n", s);
+    printf("%c\n", *(s+1));
+    
+    
+}
---
+++ 5. 메모리/실습/compare.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <cs50.h>
+
+int main(void) {
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
+ 
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
